565360c37bd64517d833b6aa9fada7245fe22b3d
Stloz <stloz.serg@gmail.com>
2023-10-10 23:21:24 +0300

    society

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
+const society = require('./society')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/society', society)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
